8c78baf1d4dbaa8ee5f597c3d1bafe2d9e182aba
Jacob <sayaomarkisrael@gmail.com>
2023-09-25 15:37:46 +0800

    VSC

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light' : "#ffffff",
+        'light_gray' : '#f5f5f5',
+        'light_light' : '#88949f',
+        'l_dark' : "#2c2c32",
+        'dark_dark' : '#25252b',
+        'primary_blue' : '#0066b8',
+        'footer_gray' : '#333333',
+      },
+      fontFamily: {
+        'Poppins': 'sans-serif',
+      },
+    },
+  },
+  plugins: [],
+};
