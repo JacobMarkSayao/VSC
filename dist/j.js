2b67f42ed6f72e8dedc5961ffd970feaead70676
Jacob <sayaomarkisrael@gmail.com>
2023-09-26 15:47:13 +0800

    Service Part

--- dist/j.js
+++ dist/j.js
@@ -4,5 +4,12 @@ const headerNav = document.querySelector(".header__nav");
 toggleBtn.addEventListener("click", () => {
   headerNav.classList.toggle("open");
   toggleBtn.classList.toggle("open");
+});
 
+const bannerBtn = document.querySelector(".chev");
+const bannerTable = document.querySelector(".drop__down");
+
+bannerBtn.addEventListener("click", () => {
+  bannerTable.classList.toggle("open");
+  bannerBtn.classList.toggle("open");
 });
